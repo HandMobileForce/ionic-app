c69acd9cd00025997cee8df6d192e11bbe41c0a3
songsong.zhang <1733458402@qq.com>
2017-04-11 11:21:40 +0800

    autoprefixer插件添加

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ var clean = require('gulp-clean');
 var notify = require('gulp-notify');//提示信息
 var gulpNgConfig = require('gulp-ng-config');//提示信息
 var ngAnnotate = require('gulp-ng-annotate');
+var autoprefixer = require('gulp-autoprefixer');
 var tinylr = require('tiny-lr');
 var fs = require('fs');
 var path = require('path');
@@ -161,16 +162,16 @@ gulp.task('html', [/*'rootHtml',*/ 'pagesHtml']);
 //复制开发环境的依赖库文件
 gulp.task('copy-dev-libs', function () {
   return gulp.src(libDevFilePath)
-    //.pipe(useref({noAssets: true}, lazypipe().pipe(sourcemaps.init, {loadMaps: true})))
-    //.pipe(sourcemaps.write('.'))
+  //.pipe(useref({noAssets: true}, lazypipe().pipe(sourcemaps.init, {loadMaps: true})))
+  //.pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('www/build/lib'));
 });
 
 //复制发布环境的依赖库文件
 gulp.task('copy-publish-libs', function () {
   return gulp.src(libPublishFilePath)
-    //.pipe(useref({noAssets: true}, lazypipe().pipe(sourcemaps.init, {loadMaps: true})))
-    //.pipe(sourcemaps.write('.'))
+  //.pipe(useref({noAssets: true}, lazypipe().pipe(sourcemaps.init, {loadMaps: true})))
+  //.pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('www/build/lib'));
 });
 
@@ -211,8 +212,8 @@ gulp.task('copy-ios-appStore-config', function () {
 
 gulp.task('copy-common-js-libs', function () {
   return gulp.src(libDevCommonFilePath)
-    //.pipe(useref({noAssets: true}, lazypipe().pipe(sourcemaps.init, {loadMaps: true})))
-    //.pipe(sourcemaps.write('.'))
+  //.pipe(useref({noAssets: true}, lazypipe().pipe(sourcemaps.init, {loadMaps: true})))
+  //.pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('www/build/common'));
 });
 
@@ -229,7 +230,18 @@ gulp.task('copy-publish-lib', function (callback) {
 //合并压缩css文件
 gulp.task('sass', function () {
   return gulp.src(['src/theme/*.scss'])
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(sourcemaps.write({includeContent: false}))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(autoprefixer({
+      browsers: ['iOS > 6', 'not ie < 6', '> 5%'],
+      cascade: true, //是否美化属性值 默认：true 像这样：
+      //-webkit-transform: rotate(45deg);
+      //        transform: rotate(45deg);
+      remove: true //是否去掉不必要的前缀 默认：true
+    }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('www/build/css'));
 });
 
@@ -304,15 +316,15 @@ gulp.task('scripts', function () {
 //
 gulp.task('copy-prod', function () {
   return gulp.src([
-      'src/**/*',
-      '!src/index.html',
-      '!src/**/*.ts',
-      '!src/**/*.less',
-      '!src/**/*.sass',
-      '!src/**/*.styl',
-      '!src/css/*',
-      '!src/**/*.md',
-      '!src/scripts/*'])
+    'src/**/*',
+    '!src/index.html',
+    '!src/**/*.ts',
+    '!src/**/*.less',
+    '!src/**/*.sass',
+    '!src/**/*.styl',
+    '!src/css/*',
+    '!src/**/*.md',
+    '!src/scripts/*'])
     .pipe(gulp.dest('www'));
 });
 
